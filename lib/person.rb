6bcb26bf62f468268e7e4c05efd1db67738a646b
Peyton Kellogg <pekellogg@hotmail.com>
2021-01-24 20:41:48 -0800

    commit tree ea47b9b61d86f95e43abd8c86b8317e170845a5f

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,37 @@
-# your code goes here
+# # your code goes here
+# class Person
+#     def initialize(name, bank_account = 25, happiness = 8)
+#         @name = name
+#         @bank_account = bank_account
+#         @happiness = happiness
+#     end
+
+#     def name
+#         @name
+#     end
+
+#     def bank_account
+#         @bank_account
+#     end
+
+#     def bank_account=(new_value)
+#         @bank_account = new_value
+#     end
+# end
+
+# ziad = Person.new("Ziad")
+
+
+class Person
+
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
+    @name, @bank_account, @happiness.clamp(0..10), @hygiene = name, bank_account, happiness, hygiene
+    end
+end
+
+
+
+
